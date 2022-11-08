ad3fcbdce67a466590d71a19849518e0e73595a9
fatiha <aydin.fatih@hepsiburada.com>
2022-11-09 01:27:36 +0300

    migrations added.

--- src/ExchangeRate.Data/Migrations/20221108222708_customer_api_keys_added.Designer.cs
+++ src/ExchangeRate.Data/Migrations/20221108222708_customer_api_keys_added.Designer.cs
@@ -4,6 +4,7 @@ using ExchangeRate.Data.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExchangeRate.Data.Migrations
 {
     [DbContext(typeof(ExchangeRateDbContext))]
-    partial class ExchangeRateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221108222708_customer_api_keys_added")]
+    partial class customerapikeysadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ExchangeRate.Data/Migrations/20221108222708_customer_api_keys_added.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExchangeRate.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class customerapikeysadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerApiKeys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ApiKey = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerApiKeys", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerApiKeys");
+        }
+    }
+}
