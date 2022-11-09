bc7d0fa2d1dd5d697ae8974e5df013388f2e82ab
fatiha <aydin.fatih@hepsiburada.com>
2022-11-09 22:31:39 +0300

    polly removed.

--- src/ExchangeRate.Infrastructure/ExternalServices/ExternalExchangeService.cs
+++ src/ExchangeRate.Infrastructure/ExternalServices/ExternalExchangeService.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Polly;
-using Polly.CircuitBreaker;
 using System.Net.Http.Headers;
 
 namespace ExchangeRate.Infrastructure.ExternalServices
@@ -16,7 +14,6 @@ namespace ExchangeRate.Infrastructure.ExternalServices
         private readonly string _baseUrl;
         private readonly string _apiKey;
         private readonly ILogger<ExternalExchangeService> _logger;
-        private readonly Policy _policy;
 
 
         public async Task<ExchangeRatesModel?> GetLatest(string? exchangeBase, string? symbols = "")
@@ -26,34 +23,30 @@ namespace ExchangeRate.Infrastructure.ExternalServices
                 exchangeBase = "EUR";
             }
 
-            await _policy.Execute( async () => {
-                using (var client = new HttpClient())
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("apikey", _apiKey);
+
+                HttpResponseMessage response = await client.GetAsync($"fixer/latest?symbols={symbols}&base={exchangeBase}");
+
+                if (response.IsSuccessStatusCode)
                 {
-                    client.BaseAddress = new Uri(_baseUrl);
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    client.DefaultRequestHeaders.Add("apikey", _apiKey);
+                    var content = response.Content.ReadAsStringAsync().Result;
 
-                    HttpResponseMessage response = await client.GetAsync($"fixer/latest?symbols={symbols}&base={exchangeBase}");
+                    var result = JsonConvert.DeserializeObject<ExchangeRatesModel>(content);
 
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var content = response.Content.ReadAsStringAsync().Result;
-
-                        var result = JsonConvert.DeserializeObject<ExchangeRatesModel>(content);
-
-                        return result;
-                    }
-                    else
-                    {
-                        _logger.LogCritical("Error from external exchange service", response.StatusCode);
-
-                        throw new Exception(response.StatusCode.ToString());
-                    }
+                    return result;
                 }
-            });
+                else
+                {
+                    _logger.LogCritical("Error from external exchange service", response.StatusCode);
 
-            return null;
+                    throw new Exception(response.StatusCode.ToString());
+                }
+            }
         }
 
         public ExternalExchangeService(string baseUrl, string apiKey, ILogger<ExternalExchangeService> logger)
@@ -61,14 +54,6 @@ namespace ExchangeRate.Infrastructure.ExternalServices
             _baseUrl = baseUrl;
             _apiKey = apiKey;
             _logger = logger;
-
-            _policy = Policy
-                .Handle<Exception>()
-                .WaitAndRetry(int.MaxValue, retryCount => TimeSpan.FromSeconds(Math.Pow(2, retryCount)),
-                    (ex, span) =>
-                    {
-                        logger.LogWarning("Unable to update bulk listing operation task.", ex);
-                    }); ;
         }
     }
 
