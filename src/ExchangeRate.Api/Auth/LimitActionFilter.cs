3cecdbdb6e0f50c3409fa468dad606ca21e0d75b
fatiha <aydin.fatih@hepsiburada.com>
2022-11-10 23:42:01 +0300

    limit action filter updated.

--- src/ExchangeRate.Api/Auth/LimitActionFilter.cs
+++ src/ExchangeRate.Api/Auth/LimitActionFilter.cs
@@ -1,15 +1,16 @@
 ﻿using ExchangeRate.Abstraction.Data;
 using ExchangeRate.Data.Data;
 using ExchangeRate.Data.DataAccess;
+using ExchangeRate.Infrastructure.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Net;
 
 namespace ExchangeRate.Api.Auth
 {
-    public class LimitActionFilter : IActionFilter
+    public class LimitActionFilter : ActionFilterAttribute
     {
-        public async void OnActionExecuting(ActionExecutingContext context)
+        public override void OnActionExecuting(ActionExecutingContext context)
         {
             var unitOfWork = context.HttpContext.RequestServices.GetService<IUnitOfWork>();
 
@@ -17,19 +18,15 @@ namespace ExchangeRate.Api.Auth
 
             var apiKey = context.HttpContext.Request.Headers["ApiKey"].FirstOrDefault();
 
-            var oldRecordsForApiKey = await repository.GetAsync(x => x.ApiKey == apiKey && x.CreatedDate > DateTime.Now.AddHours(-1) && x.Direction == Direction.Outgoing && x.HttpStatusCode == (int)HttpStatusCode.OK);
+            var oldRecordsForApiKey = repository.GetAsync(x => x.ApiKey == apiKey && x.CreatedDate > DateTime.Now.AddHours(-1) && x.Direction == Direction.Outgoing && x.HttpStatusCode == (int)HttpStatusCode.OK);
 
-            var count = oldRecordsForApiKey.Count();
+            var count = oldRecordsForApiKey.Result.Count();
 
             if (count >= 10)
             {
                 context.Result = new JsonResult(new { message = $"ApiKey: {apiKey} has requested too much!" }) { StatusCode = StatusCodes.Status429TooManyRequests };
                 return;
             }
-        }
-        public void OnActionExecuted(ActionExecutedContext context)
-        {
-            // our code after action executes
         }
     }
 }
