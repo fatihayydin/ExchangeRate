5db6286997e93f4640c9ccddd7c423a6c5d856f6
fatiha <aydin.fatih@hepsiburada.com>
2022-11-11 00:26:43 +0300

    removed index attr.

--- src/ExchangeRate.Data/Data/CustomerApiLog.cs
+++ src/ExchangeRate.Data/Data/CustomerApiLog.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExchangeRate.Data.Data
 {
-    //[Index(nameof(ApiKey),nameof(CreatedDate), nameof(HttpStatusCode), nameof(Direction))]
     public class CustomerApiLog
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
